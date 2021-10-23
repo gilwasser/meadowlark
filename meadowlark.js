85246289797d0cdfd591d3f88e3efc5baeaec1e3
gilwasser <40173646+gilwasser@users.noreply.github.com>
2021-10-23 15:33:33 +0300

    Moved 'fortune cookie' to module.

--- meadowlark.js
+++ meadowlark.js
@@ -13,21 +13,14 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-]
+const fortune = require('./lib/fortune')
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', {fortune: randomFortune})
+    res.render('about', {fortune: fortune.getFortune()})
 })
 
 
